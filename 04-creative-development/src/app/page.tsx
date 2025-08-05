1d94beef2d3f8941b72a29f8b20f36c1cf4b992f
Dargonf <dargonf.hu@gmail.com>
2025-08-05 11:03:18 +0200

    04 - added background image drawing (currently no image setup) and a small function to see were the screen is clicked for future usage for interactivity

--- 04-creative-development/src/app/page.tsx
+++ 04-creative-development/src/app/page.tsx
@@ -3,19 +3,44 @@ import { useEffect } from "react";
 import "destyle.css";
 import "./page.css";
 
+function addBackgroundImage(context: CanvasRenderingContext2D) {
+  const base_image = new Image();
+  base_image.src = "./img/placeholder.png";
+  base_image.onload = function () {
+    context.drawImage(base_image, 0, 0);
+  };
+}
+
+const drawClick = (x: number, y: number, context: CanvasRenderingContext2D) => {
+  context.beginPath();
+  context.arc(x, y, 3, 0, 2 * Math.PI);
+  context.fillStyle = "blue";
+  context.fill();
+};
+
 export default function Home() {
   useEffect(() => {
     const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 
-    const width = canvas.clientWidth * 2;
-    const height = canvas.clientHeight * 2;
-
-    canvas.width = width;
-    canvas.height = height;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
     const context = canvas.getContext("2d");
 
     if (!context) return;
+
+    const handleClick = (event: MouseEvent) => {
+      const rect = canvas.getBoundingClientRect();
+      const x = event.clientX - rect.left;
+      const y = event.clientY - rect.top;
+      drawClick(x, y, context);
+    };
+
+    canvas.addEventListener("click", handleClick);
+
+    return () => {
+      canvas.removeEventListener("click", handleClick);
+    };
   }, []);
 
   return (
